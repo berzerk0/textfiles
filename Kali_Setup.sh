ab0b38c489cdbf06803987d99dfbe10834799d0f
root <root@localhost.localdomain>
2017-10-11 00:32:16 -0400

    Ready to run?

--- Kali_Setup.sh
+++ Kali_Setup.sh
@@ -4,7 +4,21 @@ apt-get update && apt-get upgrade
 apt-get install exiftool
 apt-get install gimp
 apt-get install audacity
+apt-get install libreoffice
+apt-get install terminator
+apt-get install bleachbit
 
+
+
+
+#WORDLISTS
+mkdir ~/Wordlists && cd ~/Wordlists
+wget https://github.com/berzerk0/textfiles/raw/master/some_wordlists.tar.gz
+tar xzf some_wordlists.tar.gz && rm some_wordlists.tar.gz
+
+#Generators
+mkdir ~/Generators && cd ~/Generators
+wget https://github.com/berzerk0/textfiles/raw/master/Generators.tar.gz && tar xzf Generators.tar.gz && rm Generators.tar.gz
 
 #OPT FOLDER
 
@@ -12,7 +26,7 @@ apt-get install audacity
 mkdir /opt/Hashing_Tools && cd /opt/Hashing_Tools
 
 git clone https://github.com/psypanda/hashID
-git  clone https://github.com/UltimateHackers/Hash-Buster
+git clone https://github.com/UltimateHackers/Hash-Buster
 
 
 ##OBFUSCATION_TOOLS
@@ -28,36 +42,36 @@ git clone https://github.com/liftoff/pyminifier
 mkdir /opt/Steganography_Tools && cd /opt/Steganography_Tools
 
 git clone https://github.com/alexadam/img-encode
-wget http://www.darkside.com.au/snow/snow-20130616.tar.gz && tar xzf snow-20130616.tar.gz && rm snow-20130616.tar.gz && mv snow-20130616 snow
 git clone https://github.com/abeluck/stegdetect
+
+wget http://www.darkside.com.au/snow/snow-20130616.tar.gz && tar xzf snow-20130616.tar.gz && rm snow-20130616.tar.gz && mv snow-20130616 snow
+
 
 mkdir stegsolve && cd stegsolve
 wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar && chmod +x stegsolve.jar
+
+apt-get install stegosuite
 
 
 ##OSINT_TOOLS
 mkdir /opt/OSINT_Tools && cd /opt/OSINT_Tools
 
 git clone https://github.com/mschwager/gitem
-git clonehttps://github.com/WebBreacher/WhatsMyName
-
-mkdir spiderfoot && cd spiderfoot
-wget http://sourceforge.net/projects/spiderfoot/files/spiderfoot-2.3.0-src.tar.gz/download && tar xzvf download
-pip install lxml && pip install netaddr && pip install M2Crypto && pip install cherrypy && pip install mako
+git clone https://github.com/WebBreacher/WhatsMyName
 
 ###Email_OSINT
-mkdir /opt/Email_OSINT && cd /opt/Email_OSINT
+mkdir /opt/OSINT_Tools/Email_OSINT && cd /opt/OSINT_Tools/Email_OSINT
 git clone https://github.com/laramies/theHarvester
 git clone https://github.com/WebBreacher/emailwhois
 git clone https://github.com/m4ll0k/Infoga.git && cd Infoga && pip install -r requirements.txt && cd ..
 
 
 ###Facebook_OSINT
-mkdir /opt/Facebook_OSINT && cd /opt/Facebook_OSINT
+mkdir /opt/OSINT_Tools/Facebook_OSINT && cd /opt/OSINT_Tools/Facebook_OSINT
 git clone https://github.com/guelfoweb/fbid
 
 ###LinkedIn_Osint
-mkdir /opt/LinkedIn_OSINT && cd /opt/LinkedIn_OSINT
+mkdir /opt/OSINT_Tools/LinkedIn_OSINT && cd /opt/OSINT_Tools/LinkedIn_OSINT
 git clone https://github.com/0x09AL/raven
 git clone https://github.com/mdsecactivebreach/LinkedInt
 
@@ -65,6 +79,11 @@ git clone https://github.com/mdsecactivebreach/LinkedInt
 mkdir /opt/OSINT_Tools/Domain_OSINT && cd /opt/OSINT_Tools/Domain_OSINT
 git clone https://github.com/1N3/Sn1per
 git clone https://github.com/Smaash/snitch
+
+
+mkdir /opt/OSINT_Tools/spiderfoot && cd /opt/OSINT_Tools/spiderfoot
+wget http://sourceforge.net/projects/spiderfoot/files/spiderfoot-2.3.0-src.tar.gz/download && tar xzvf download
+pip install lxml && pip install netaddr && pip install M2Crypto && pip install cherrypy && pip install mako
  
 
 
@@ -77,20 +96,20 @@ git clone https://github.com/nccgroup/shocker
 
 
 #SHELL_TOOLS
-mkdir /opt/Shell_Tools cd /opt/Shell_Tools
-
+mkdir /opt/Shell_Tools && cd /opt/Shell_Tools
 git clone https://github.com/infodox/python-pty-shells
-ln -s /usr/share/webshells/ /opt/Exploit_Tools/webshells
 
 
 mkdir /opt/Shell_Tools/Web_Shells && cd /opt/Shell_Tools/Web_Shells
-https://github.com/b374k/b374k
+git clone https://github.com/b374k/b374k
+
+ln -s /usr/share/webshells/ /opt/Shell_Tools/Web_Shells/webshells
 
 
 
 
 #PASSWORDING_TOOLS
-mkdir /opt/PasswordCredential_Tools cd /opt/PasswordCrendtial_Tools
+mkdir /opt/PasswordCredential_Tools && cd /opt/PasswordCrendential_Tools
 
 git clone https://github.com/berzerk0/BEWGor
 git clone https://github.com/lightos/credmap
@@ -112,7 +131,12 @@ git clone https://github.com/Dionach/CMSmap
 git clone https://github.com/nahamsec/CMSFuzz
 git clone https://github.com/droope/droopescan
 
-  
+
+#WIRELESS_TOOLS
+mkdir /opt/Wireless_Tools && cd /opt/Wireless_Tools
+git clone https://github.com/wi-fi-analyzer/fluxion
+git clone https://github.com/DanMcInerney/wifijammer
+
 
 
 
