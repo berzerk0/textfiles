95f8c293bad2f0f6b661da5988d528d866d98ee3
Ben <berzerk0.burrito@gmail.com>
2017-10-23 16:26:18 -0400

    Additions

--- Local-Enumerator.sh
+++ Local-Enumerator.sh
@@ -49,20 +49,120 @@ echo "   >>> List of World Writeable FOLDERS written to $(pwd)/.world_writable_D
 find / ! -path "*/proc/*" -perm -2 -type f -print 2>/dev/null | tr ' ' '\n' | sort > .world_writable_files_HINKYPUNK
 echo "   >>> List of World Writeable FILES (except in /proc/) written to $(pwd)/.world_writable_FILES_HINKYPUNK"
 
+#find / -type f -name '.*' '(' -exec test -r '{}' \; ')' -print 2>/dev/null #show all hidden files the user can read
+#find / -type d -name '.*' '(' -exec test -r '{}' \; ')' -print 2>/dev/null #show all hidden dirs the user can read
+#if these return files that cannot be read - find may be running as root
+
+
+
+if [ -d "/home/$(whoami)" ]; then
+echo "[+] nc available
+else 
+echo " [-] nc not found"
+fi
+
+
+
 echo '------HANDY PROGRAMS------'
 
-which nc
-which vi
-which vim
+#nc
+hinkp_nc_exists=`which nc 2>/dev/null`
+if [ "$hinkp_nc_exists" ]; then
+echo "[+] nc available
+else 
+echo " [-] nc not found"
+fi
+unset hinkp_nc_exists
+
+#ncat
+hinkp_ncat_exists=`which ncat 2>/dev/null`
+if [ "$hinkp_ncat_exists" ]; then
+echo "[+] ncat available
+else 
+echo " [-] ncat not found"
+fi
+unset hinkp_ncat_exists
+
+#netcat
+hinkp_netcat_exists=`which netcat 2>/dev/null`
+if [ "$hinkp_ncat_exists" ]; then
+echo "[+] netcat available
+else 
+echo " [-] netcat not found"
+fi
+unset hinkp_netcat_exists
+
+#Python
+hinkp_python_ver=`python -V 2>/dev/null`; 
+if [ "$hinkp_python_ver" ]; then
+python -V
+else 
+echo " [-] Python not found"
+fi
+unset hinkp_python_ver
+
+#mysql
+hinkp_mysql_ver=`mysql --version 2>/dev/null`
+if [ "$hinkp_mysql_ver" ]; then
+echo "[+] mysql : $(mysql --version | tr -s ' ' | cut -d' ' -f3-)
+else 
+echo " [-] mysql not found"
+fi
+unset hinkp_mysql_ver
+
+
+#check for gcc
+hinkp_gcc_ver=`gcc -v 2>/dev/null`
+if [ "$hinkp_gcc_ver" ]; then
+
+else 
+echo " [-] gcc not found"
+fi
+unset hinkp_gcc_ver
+
+#mysql
+hinkp_mysql_ver=`mysql --version 2>/dev/null`
+if [ "$hinkp_mysql_ver" ]; then
+echo "[+] mysql : $(mysql --version | tr -s ' ' | cut -d' ' -f3-)
+else 
+echo " [-] mysql not found"
+fi
+unset hinkp_mysql_ver
+
+
+#Perl
+hinkp_perl_ver=`perl -v 2>/dev/null`
+if [ "$hinkp_perl_ver" ]; then
+echo " [+] Perl: $(perl -v | egrep -o 'v([0-9]\.*)+')"
+else 
+echo " [-] Perl not found"
+fi
+unset hinkp_perl_ver
 
 
 
- 
-#software versions
+#Java
+hinkp_java_ver=`java -version 2>/dev/null`; 
+if [ "$hinkp_java_ver" ]; then
+java -version
+else 
+echo " [-] Java not found"
+fi
+unset hinkp_java_ver
+
+#Ruby
+hinkp_ruby_ver=`ruby -v 2>/dev/null`; 
+if [ "$hinkp_ruby_ver" ]; then
+echo " [+] Ruby: $(ruby -v | cut -d' ' -f2)"
+else 
+echo " [-] Ruby not found"
+fi
+unset hinkp_ruby_ver
+
 
 #gcc -v 
-#mysql --version
-#perl -v
-#ruby -v
-#python --version
-#java -version
+
+
+#echo '------SSH KEYS------'
+# ls /home/*/.ssh/
+
