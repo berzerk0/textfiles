c2e8d11cee3b6eca8d1df6686d1449ae1461fe97
Ben <berzerk0.burrito@gmail.com>
2018-02-14 21:30:35 -0500

    Create fofao.sh

---
+++ reference_txts/fofao.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+printf "                        'dddh.  -odhdy                      
+                  '.-://+smmhmoydhhmd::-.'                  
+             ./syhdddhhhhhhddhdhyyhhhhhddddys/.             
+         '/sddhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhdds:'         
+       /hdhhhhhyyyyyyyyyyyyyyyyyyyyyyyyhyyyyyyyyhdds-       
+      'odmNNNmdyyyyyyyyyhhyyyyyhhhyyyyyhhyyyyyyyhhyhdh/     
+       -hddhyyyyyyyyyyyyyhyyyyyyyyyyyyyyyyyyyyyyhhhyyhdh'   
+     +mdhyyyyyyyyyyyyyyyyyhhyyyhyyyyyyyyyyyhhyyyyhmmmddm:   
+   :ddhyyyyhhyyyyhhyyyyyyymhydhhhyyyyhhhyyyyyyyyyyhhmh.     
+  omhyyyyyyyyyyyyhhyyyhhyhN:::oyhddhhhyyyyyhhhhhyyyyydd-    
+ omhyyhyyyyyyyyyyyyyhmhyhNo:::::::oydmdhyydNmmmdhhhyyyhm/   
+-Nhyyhhyyyyyhyyyhhyhmdmdd+::::::::/:::/+:::dmdddhhyyyyyhN:  
+ydyhddyyhhhhmhyyhyhmsoo+::::::::::::+/:/::::ymddddhyyyyyhm. 
+omhNNhyyyhydhyyhhdy:::://:::::::::+:   ':o:::+dmdddhhyyyyms'
+ /+hmyyyyyymdhhmy/:::+:' '-+:::::::   +omds::::odmdddhyyyhm-
+   hhyyyyyyhmmmN/:::+'  :oyo+::::/-  'dmNNm::::::odmddyyyhN+
+   NhyyhyyhdddmN/:::/  .myNNh::/::/'  +mNNm::::::::+ymdhyymo
+  'Nhyhhyyhdddmd::::o  'dNNNh:////:+:. ./oo:::::::::::smhymo
+   mhyyyyhddddN+::::+/' '+sh/::/++/:://:/::::::::::::::smhN/
+   smyyyyhdddNh:::::::+//++:::os/:+o+++::::::::::::::::/dNd.
+   -Nhyyyhddmm::::/:/:::::::oso/::::::+y:::::::::::::::/ym- 
+    ydhhyhddNo:///:::::::::/y/://:/+sssm/::::::::::::::/hs  
+    'mhhhddmm//:::::::::::::sssyssssoydNd/:::::::::::::om-  
+     :mhhMmmh::::::::::::::::mdhhddmmmmmNd+/:::::::::/od:   
+      -ymydNd/::::::::::::::/mNmmddhhhhdmmdhs+//////+/-     
+          'hMs/:::::::::::::+Nymmhhhssyhyd.'./soo++/'       
+            :yy/::::::::::/+dhhyso+///oyh-                  
+              :ss/::::://+ydo:-:++///+/.                    
+                '-///:///+-                                 \n"
