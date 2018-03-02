178570f1e57c353da529a8cdcf0af5c5f66500b6
root <root@localhost.localdomain>
2018-03-02 15:25:57 -0500

    restructing

---
+++ shell_scripts/benmap/benmap_fast_multi.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#./benmap_fast_multi.sh FILENAME.txt
+
+#FILE must be in "ip,hostname" format
+
+
+while read -r line
+do
+
+  ip_addr=$(echo $line | cut -d ',' -f 1)
+  box_name=$(echo $line | cut -d ',' -f 2)
+
+  #Alias for output directory
+  benmap_dir="$HOME/CTFs/$box_name"
+
+  # If output directory does not exist, create and move into it.
+  if [ ! -d "$benmap_dir" ]; then
+  	mkdir ~/CTFs/$box_name && cd ~/CTFs/$box_name || return
+  	touch "NOTES_$box_name.txt"
+  else
+    cd ~/CTFs/$box_name || return
+  fi
+
+  #If output dir does exist, but not NOTES file, create notes file.
+  if [ ! -f "$HOME/CTFs/NOTES_$box_name.txt" ]; then
+  	touch "NOTES_$box_name.txt"
+  fi
+
+  nmap -F -T4 "$ip_addr" -oA nmap_"$box_name"_fastscan
+
+
+
+
+done < "$1"
---
+++ shell_scripts/post-scan/tau_block.sh
@@ -0,0 +1,17 @@
+#WORK IN PROGRESS - DO NOT USE
+
+
+#!/bin/sh
+
+
+
+
+# runs a few commands to clear tracks
+
+
+#1 = connecting ip_address - lines to be removed from logfiles
+
+
+#server logs
+#ssh logs
+#copy history file, copy log file
