e6f7e47153a3c67d6ec9a74161881e517c001595
berzerk0 <bfberko@icloud.com>
2018-03-03 12:05:49 -0500

    generator scripts work in progress

---
+++ generators/US_female_name.sh
@@ -0,0 +1,16 @@
+FIRST=sources/US_female_first_names.txt
+SECOND=sources/US_last_names.txt
+
+count_first=$(wc -l < $FIRST);
+count_second=$(wc -l < $SECOND);
+
+choose_first=$((RANDOM%count_first+0));
+choose_second=$((RANDOM%count_second+0));
+
+first_word=$(head -n $choose_first $FIRST | tail -n 1);
+second_word=$(head -n $choose_second $SECOND | tail -n 1);
+
+echo " "
+echo $first_word
+echo $second_word
+echo " "
