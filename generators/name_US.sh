94e0363dc530d0094ef59a746c6e17ce9a15179f
root <root@localhost.localdomain>
2018-03-04 02:13:19 -0500

    rearranging, adding sources, better scripting

--- generators/name_US.sh
+++ generators/name_US.sh
@@ -1,5 +1,5 @@
-FIRST=sources/US_first_names.txt
-SECOND=sources/US_last_names.txt
+FIRST=sources/names/US_first_names.txt
+SECOND=sources/names/US_last_names.txt
 
 count_first=$(wc -l < $FIRST);
 count_second=$(wc -l < $SECOND);
@@ -10,7 +10,4 @@ choose_second=$((RANDOM%count_second+0));
 first_word=$(head -n $choose_first $FIRST | tail -n 1);
 second_word=$(head -n $choose_second $SECOND | tail -n 1);
 
-echo " "
-echo $first_word
-echo $second_word
-echo " "
+echo "US name: $first_word $second_word"
--- shell_scripts/obfuscation_and_doubt/CombObfuscator.sh
+++ shell_scripts/obfuscation_and_doubt/CombObfuscator.sh
@@ -8,11 +8,18 @@
 # chmod u+x CombObfuscator.sh; ./CombObfuscator.sh script.py
 #######################################################
 
-enc_options=(simple_ascii simple_hex_rev); #Obfuscation operations
+#This only works on python scripts!
+
+RED='\033[0;31m'
+enc_options=(simple_ascii simple_hex_rev simple_base64_rev); #Obfuscation operations
 OUTFILE=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w$(( (RANDOM % 15) +10 )) | head -n 1)
 
 cp $1 $OUTFILE #creates randomly named copy for outfile
 
+if [ $# -ne 1 ]; then
+	printf " ${RED}Usage: ${NC} ./CombObfuscator.sh script.py\n"
+	exit 1
+fi
 
 #Function to choose obfuscation method at random, then minify.
 obfuscateAndMinify () {
@@ -25,32 +32,38 @@ obfuscateAndMinify () {
 	cat $OUTFILE | tail +$lines > NAzQ31tFT9EzIYrX8Xqtempfile
 
 	#Minify adding obfuscation and aliases.
-	pyminifier -O --replacement-length=$(( (RANDOM % 10) +1 )) NAzQ31tFT9EzIYrX8Xqtempfile | grep -vi 'pyminifier' > $OUTFILE
+	#pyminifier -O --replacement-length=$(( (RANDOM % 10) +1 )) NAzQ31tFT9EzIYrX8Xqtempfile | grep -vi 'pyminifier' > $OUTFILE
 
-	#Repeat process 
+	#Repeat process
 	lines=$(expr $(wc -l < $OUTFILE) + 4)
 	zsc -p python/${enc_options[$((RANDOM % 1 ))]} -i $OUTFILE >> /dev/null #call ZSC for obfuscation
 	cat $OUTFILE | tail +$lines > NAzQ31tFT9EzIYrX8Xqtempfile
 
-	mv NAzQ31tFT9EzIYrX8Xqtempfile $OUTFILE	
+	mv NAzQ31tFT9EzIYrX8Xqtempfile $OUTFILE
 }
 
 obfuscateAndMinify #run function (Put in function so it can be looped)
 
 
 # add random "Comments" at beggining of script
-for i in `seq 1 $(( (RANDOM % 5) + 5 ))`; do echo $"#$(cat /dev/urandom | tr -dc 'A-Za-z0-9!@#%^&*()_+-=' | fold -w$(( (RANDOM % 51) +10 )) | head -n 1)" >> NAzQ31tFT9EzIYrX8Xqtempfile; done
+for i in $(seq 1 $(( (RANDOM % 5) + 5 )));
+	do echo $"# $(cat /dev/urandom | tr -dc 'A-Za-z0-9!@#%^&*()_+-=' | fold -w$(( (RANDOM % 51) +10 )) | head -n 1))" >> NAzQ31tFT9EzIYrX8Xqtempfile;
+done
 
 #add random "Comments" at end of script, and an a random md5sum for no reason.
-echo $'\n' >> qE2wNc8386Dep4BSBRYdtempfile 
-echo $"#$(cat /dev/urandom | tr -dc 'A-Za-z0-9!@#$%^&*()_+-=][.,<>/?' | fold -w$(( (RANDOM % 50) +10 )) | head -n 1 | md5sum | cut -d ' ' -f 1)" >> qE2wNc8386Dep4BSBRYdtempfile
-for i in `seq 1 $(( (RANDOM % 3) + 3 ))`; do echo $"#$(cat /dev/urandom | tr -dc 'A-Za-z0-9!@#%^&*()_+-=' | fold -w$(( (RANDOM % 51) +10 )) | head -n 1)" >> qE2wNc8386Dep4BSBRYdtempfile; done
+echo $'\n' >> qE2wNc8386Dep4BSBRYdtempfile
+echo $"# $(cat /dev/urandom | tr -dc 'A-Za-z0-9!@#$%^&*()_+-=][.,<>/?' | fold -w$(( (RANDOM % 50) +10 )) | head -n 1 | md5sum | cut -d ' ' -f 1)" >> qE2wNc8386Dep4BSBRYdtempfile
+for i in $(seq 1 $(( (RANDOM % 3) + 3 )));
+	do echo $"# $(cat /dev/urandom | tr -dc 'A-Za-z0-9!@#%^&*()_+-=' | fold -w$(( (RANDOM % 51) +10 )) | head -n 1)" >> qE2wNc8386Dep4BSBRYdtempfile;
+done
 
 
 #Finalize
 echo $'\n' | cat NAzQ31tFT9EzIYrX8Xqtempfile - $OUTFILE qE2wNc8386Dep4BSBRYdtempfile > zDeiyUNsbwtempfile && mv zDeiyUNsbwtempfile $OUTFILE
 
+printf "CombObfuscator created $OUTFILE at $(date | cut -d ' ' -f 5) \n"
+
+
+
 #Remove tempfiles
 rm NAzQ31tFT9EzIYrX8Xqtempfile qE2wNc8386Dep4BSBRYdtempfile
-
-
