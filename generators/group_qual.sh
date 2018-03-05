54e822e40b5fc9f2b8138628b6f478c24ce3244e
berzerk0 <bfberko@icloud.com>
2018-03-04 19:06:05 -0500

    qual_group script

---
+++ generators/group_qual.sh
@@ -0,0 +1,15 @@
+FIRST=sources/locations/Qual_Cities.txt
+SECOND=sources/parts_of_speech/nouns_of_assembly.txt
+
+count_first=$(wc -l < $FIRST);
+count_second=$(wc -l < $SECOND);
+
+choose_first=$((RANDOM%count_first+0));
+choose_second=$((RANDOM%count_second+0));
+
+first_word=$(head -n $choose_first $FIRST | tail -n 1);
+second_word=$(head -n $choose_second $SECOND | tail -n 1);
+
+
+echo "Qual Group: $first_word $second_word"
+
