1773e92723ab4f2cf46c7070c02ec2f9eba94db8
root <root@localhost.localdomain>
2019-02-27 15:43:56 -0500

    aliases and notes

--- that_vm/sync_27Feb.sh
+++ that_vm/sync_27Feb.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
+
+#Set Up vstftp and configuration
+apt-get install vstftpd
+cp /etc/vsftpd.conf /etc/original_vstfpd.conf
+wget https://raw.githubusercontent.com/berzerk0/textfiles/master/reference_txts/anonymous_vsftpd.conf -O /etc/vsftpd.conf
+
+
+#add bash alias for tftp
+
+#alias start-tftp="if [ -d '/tmp/ftproot' ]; then atftpd --daemon --port 69 /tmp/ftproot && echo 'tftp service started'; else mkdir '/tmp/ftproot' && atftpd --daemon --port 69 /tmp/ftproot && echo 'tftp service started'; fi"
+
+#alias for nc to ncat
+#alias nc="ncat"
+
+#alias netcat to ncat
+#alias netcat="ncat"
+
+
 # set up bash aliases
-echo "CiMgIkhvbWUiIERpcmVjdG9yeSBvbiBVU0IKYWxpYXMgdm1kaXI9ImNkIC9tZWRpYS9yb290L1RI
-VU1CRVJLTy9PU0NQLVBXSy9TaGFyZV9QV0siCgojIGRpcnNlYXJjaCBzaG9ydGN1dAphbGlhcyBk
-aXJzZWFyY2g9Ii9vcHQvV2ViX1Rvb2xzL2RpcnNlYXJjaC9kaXJzZWFyY2gucHkiCgojIGRpcnNl
-YXJjaC1xdWljayB1c2VzIGNvbW1vbiB3b3JkbGlzdAphbGlhcyBkaXJzZWFyY2gtcXVpY2s9Ii9v
-cHQvV2ViX1Rvb2xzL2RpcnNlYXJjaC9kaXJzZWFyY2gucHkgLS13b3JkbGlzdD0vdXNyL3NoYXJl
-L3dvcmRsaXN0cy9kaXJiL2NvbW1vbi50eHQiCgojIGRpcnNlYXJjaC1tZWRpdW0gdXNlcyAibWVk
-aXVtIiAoYWN0dWFsbHkgbGFyZ2UpIHdvcmRsaXN0CmFsaWFzIGRpcnNlYXJjaC1tZWRpdW09Ii9v
-cHQvV2ViX1Rvb2xzL2RpcnNlYXJjaC9kaXJzZWFyY2gucHkgLS13b3JkbGlzdD0vdXNyL3NoYXJl
-L3dvcmRsaXN0cy9kaXJidXN0ZXIvZGlyZWN0b3J5LWxpc3QtMi4zLW1lZGl1bS50eHQiCgoKI2hh
-c2hidXN0ZXIKYWxpYXMgaGFzaGJ1c3Rlcj0icHl0aG9uMyAvb3B0L0hhc2hpbmdfVG9vbHMvSGFz
-aC1CdXN0ZXIvaGFzaC5weSIKCiNuYyB0byBuY2F0CmFsaWFzIG5jPSJuY2F0IgoKI25ldGNhdCB0
-byBuY2F0CmFsaWFzIG5ldGNhdD0ibmNhdCIK" | base64 -d > ~/.bash_aliases
+echo "I0N1cnJlbnQgYXMgb2YgMjcgRmViCgojICJIb21lIiBEaXJlY3Rvcnkgb24gVVNCCmFsaWFzIHZt
+ZGlyPSJjZCAvbWVkaWEvcm9vdC9USFVNQkVSS08vT1NDUC1QV0svU2hhcmVfUFdLIgoKIyBkaXJz
+ZWFyY2ggc2hvcnRjdXQKYWxpYXMgZGlyc2VhcmNoPSIvb3B0L1dlYl9Ub29scy9kaXJzZWFyY2gv
+ZGlyc2VhcmNoLnB5IgoKIyBkaXJzZWFyY2gtcXVpY2sgdXNlcyBjb21tb24gd29yZGxpc3QKYWxp
+YXMgZGlyc2VhcmNoLXF1aWNrPSIvb3B0L1dlYl9Ub29scy9kaXJzZWFyY2gvZGlyc2VhcmNoLnB5
+IC0td29yZGxpc3Q9L3Vzci9zaGFyZS93b3JkbGlzdHMvZGlyYi9jb21tb24udHh0IgoKIyBkaXJz
+ZWFyY2gtbWVkaXVtIHVzZXMgIm1lZGl1bSIgKGFjdHVhbGx5IGxhcmdlKSB3b3JkbGlzdAphbGlh
+cyBkaXJzZWFyY2gtbWVkaXVtPSIvb3B0L1dlYl9Ub29scy9kaXJzZWFyY2gvZGlyc2VhcmNoLnB5
+IC0td29yZGxpc3Q9L3Vzci9zaGFyZS93b3JkbGlzdHMvZGlyYnVzdGVyL2RpcmVjdG9yeS1saXN0
+LTIuMy1tZWRpdW0udHh0IgoKI2hhc2hidXN0ZXIKYWxpYXMgaGFzaGJ1c3Rlcj0icHl0aG9uMyAv
+b3B0L0hhc2hpbmdfVG9vbHMvSGFzaC1CdXN0ZXIvaGFzaC5weSIKCiN0ZnRwIHF1aWNrIHN0YXJ0
+CmFsaWFzIHN0YXJ0LXRmdHA9ImlmIFsgLWQgJy90bXAvZnRwcm9vdCcgXTsgdGhlbiBhdGZ0cGQg
+LS1kYWVtb24gLS1wb3J0IDY5IC90bXAvZnRwcm9vdCAmJiBlY2hvICd0ZnRwIHNlcnZpY2Ugc3Rh
+cnRlZCc7IGVsc2UgbWtkaXIgJy90bXAvZnRwcm9vdCcgJiYgYXRmdHBkIC0tZGFlbW9uIC0tcG9y
+dCA2OSAvdG1wL2Z0cHJvb3QgJiYgZWNobyAndGZ0cCBzZXJ2aWNlIHN0YXJ0ZWQnOyBmaQoKI25j
+IHRvIG5jYXQKYWxpYXMgbmM9Im5jYXQiCgojbmV0Y2F0IHRvIG5jYXQKYWxpYXMgbmV0Y2F0PSJu
+Y2F0Igo=" | base64 -d > ~/.bash_aliases
 
 
 echo "Synched 27 Feb Version at	$(date)" >> "/root/sync_status.txt"
