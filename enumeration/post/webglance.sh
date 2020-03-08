c7f75173a0c348ed1600452f040abe9cd3551a8f
root <root@localhost.localdomain>
2020-03-08 14:21:14 -0400

    webglance fix yet again

--- enumeration/post/webglance.sh
+++ enumeration/post/webglance.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-## "All passive"
-## uses DNS requests to public servers and queries to publicly available APIs
-
 ## $1 = the "output" name
 
 # Display error if not enough arguments given
 if [ $# -ne 1 ]; then
 	printf "Usage: ./webglance.sh '[Output Name]'\n \
-Where 'Output Name' is a suffix you want to apply to generated files"
+Where 'Output Name' is a suffix you want to apply to generated files.\n"
 	exit 1
 fi
 
@@ -16,7 +13,8 @@ fi
 ## is the EyeWitness binary present?
 ##
 gotEyeWitness=$(command -v eyewitness 2>/dev/null)
-if [ ! "$gotEyeWitness" ]; then
+if [ ! "$gotEyeWitness" ]
+then
 	printf " [-] Error: EyeWitness binary not in PATH\n \
 On Kali, it can be installed via apt-get install eyewitness'\n\n"
 	exit 1
@@ -24,15 +22,19 @@ fi
 unset gotEyeWitness
 
 
-amass_ResolvedSubdomainOutput="amass_ResolvedSubdomains_$1.txt"
+amass_RawSubdomainOutput="amass_ResolvedSubdomains_$1.txt"
 eyewitnessDir="$(pwd)/EyeWitness_Report_$1"
 
-# does the found domain file exist?
-if [ ! -f "$amass_ResolvedSubdomainOutput" ] || \
-[ ! -s "$amass_ResolvedSubdomainOutput" ]
+
+## assumes that in the input file comes from "subdomains_allpassive.sh"
+hostFile="$amass_RawSubdomainOutput"
+#change this to use a different file
+
+# does the host file exist?
+if [ ! -f "$hostFile" ] || [ ! -s "$hostFile" ]
 then
     printf "\n [-] Error: %s host file not found in current directory,\
- or is empty\n"  "$amass_ResolvedSubdomainOutput"
+ or is empty\nCheck the host file on line 30\n"  "$hostFile"
     exit 1
 fi
 
@@ -47,9 +49,13 @@ fi
 ## Eyewitness worst case scenario we get "unable to screenshot"
 ## Use this if no portscanning allowed
 eyewitness --web --max-retries 3 -d "$eyewitnessDir" \
--f "$amass_ResolvedSubdomainOutput" --prepend-https --no-prompt
+-f "$hostFile" --prepend-https --no-prompt
+
+
+printf "\n [+] EyeWitness complete!\n Find the report at\n %s\n" \
+"$eyewitnessDir/report.html"
 
 ## open the html report
-firefox "$eyewitnessDir/report.html" &
+#firefox "$eyewitnessDir/report.html" &
 
-unset amass_RawSubdomainOutput eyewitnessDir
+unset amass_RawSubdomainOutput eyewitnessDir hostFile
